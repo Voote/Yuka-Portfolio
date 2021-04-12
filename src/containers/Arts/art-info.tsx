833db8da5a7ee8ae178c9368357c627a61d9e538
Kuba Krzywanski <kszygy@gmail.com>
2021-04-12 19:35:24 +0100

    art-info moved to Arts index

--- src/containers/Arts/art-info.tsx
+++ src/containers/Arts/art-info.tsx
@@ -2,13 +2,13 @@ import '../../assets/styles.scss';
 import { labels } from '../../assets/labels';
 import { Box } from '@material-ui/core';
 
-const ArtInfo = ({ imgClass, artName, artDate }) => (
+const ArtInfo = ({ imgClass, element }) => (
   <Box display="flex">
     <div className={imgClass} />
     <div className="labels__logo labels__uppercase labels__light position__labels--info element element__child">
       <span className=" labels__logo--gold">{labels.viewDetails}</span>
-      <h4>{artName}</h4>
-      <span className="element__dimmed">{artDate}</span>
+      <h4>{element.name}</h4>
+      <span className="element__dimmed">{element.date}</span>
     </div>
   </Box>
 );
--- src/containers/Arts/index.tsx
+++ src/containers/Arts/index.tsx
@@ -1,9 +1,9 @@
 import { Link } from 'react-router-dom';
-import { Grid } from '@material-ui/core';
+import { Box, Grid } from '@material-ui/core';
 
-import ArtInfo from './art-info';
 import Navbar from '../../components/navbar';
 import Footer from '../../components/footer';
+import { labels } from '../../assets/labels';
 import { images } from './background-images';
 import '../../assets/styles.scss';
 
@@ -12,22 +12,26 @@ const Arts = () => (
     <Navbar />
     <div className="position__gallery">
       <Grid container>
-        {images.map((element) => {
-          const imgClass = `background__img ${element.class} element element__parent`;
-          return (
-            <Grid item key={element.id} xs={4}>
-              <div className="element__gallery">
-                <Link to={`/artpage/${element.id}`}>
-                  <ArtInfo
-                    imgClass={imgClass}
-                    artName={element.name}
-                    artDate={element.date}
+        {images.map((element) => (
+          <Grid item key={element.id} xs={4}>
+            <div className="element__gallery">
+              <Link to={`/artpage/${element.id}`}>
+                <Box display="flex">
+                  <div
+                    className={`background__img ${element.class} element element__parent`}
                   />
-                </Link>
-              </div>
-            </Grid>
-          );
-        })}
+                  <div className="labels__logo labels__uppercase labels__light position__labels--info element element__child">
+                    <span className=" labels__logo--gold">
+                      {labels.viewDetails}
+                    </span>
+                    <h4>{element.name}</h4>
+                    <span className="element__dimmed">{element.date}</span>
+                  </div>
+                </Box>
+              </Link>
+            </div>
+          </Grid>
+        ))}
       </Grid>
     </div>
     <Footer />
